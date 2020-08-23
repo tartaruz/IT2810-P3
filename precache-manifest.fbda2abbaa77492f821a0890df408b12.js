f8036ef6d3f71590cf14ab2a586ffd7bf79c7ee7
Thomas 'Tartarux' Ramirez <thomasrf96@gmail.com>
2020-08-23 22:13:22 +0200

    Try github page

---
+++ precache-manifest.fbda2abbaa77492f821a0890df408b12.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0ff5fcf37d8e15a1c8e6d0905357622",
+    "url": "/project-3/index.html"
+  },
+  {
+    "revision": "f105235ca8c9b28bdbe7",
+    "url": "/project-3/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "be1837e2b0a5a4a05d90",
+    "url": "/project-3/static/css/main.2bf6af1a.chunk.css"
+  },
+  {
+    "revision": "f105235ca8c9b28bdbe7",
+    "url": "/project-3/static/js/2.7948515c.chunk.js"
+  },
+  {
+    "revision": "e1455ef4d2cc4653e345017fe06fdf9e",
+    "url": "/project-3/static/js/2.7948515c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be1837e2b0a5a4a05d90",
+    "url": "/project-3/static/js/main.fefb35a8.chunk.js"
+  },
+  {
+    "revision": "655c884e06bfa2ec7256",
+    "url": "/project-3/static/js/runtime-main.dd52e80f.js"
+  },
+  {
+    "revision": "2b2c1397afa61c0f449e0b8676033a47",
+    "url": "/project-3/static/media/mongoflix.2b2c1397.png"
+  }
+]);
